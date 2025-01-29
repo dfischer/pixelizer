60cbfbd353700e52847ab7deae7cf425c83b1e81
Igor Zinken <igor.zinken@gmail.com>
2025-01-29 19:19:28 +0100

    Update copyright mention

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -2,7 +2,6 @@
  * The MIT License (MIT)
  *
  * Igor Zinken 2025 - https://www.igorski.nl
- * Adapted from work by Matt Kandler (https://www.mattkandler.com/blog/duotone-image-filter-javascript-rails)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
